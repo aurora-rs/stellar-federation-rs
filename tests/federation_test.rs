fa650d328707b1f2bd388c05f410420db8417c64
Francesco Ceccon <francesco@ceccon.me>
2021-01-15 19:56:24 +0000

    release: v0.2.0
    Signed-off-by: Francesco Ceccon <francesco@ceccon.me>

--- tests/federation_test.rs
+++ tests/federation_test.rs
@@ -2,7 +2,7 @@ use stellar_federation::resolve_stellar_address;
 
 #[tokio::test]
 async fn test_federation() {
-    let no_memo = resolve_stellar_address("francesco*ceccon.me")
+    let no_memo = resolve_stellar_address("francesco*stellar.ceccon.me")
         .await
         .unwrap();
 
@@ -11,19 +11,19 @@ async fn test_federation() {
         "GBUFHFEIMKTBQQFDSCAZFOC6MAUE3EHBVE4S4RYKMX62PMWDIDSD44CP"
     );
 
-    let with_memo_text = resolve_stellar_address("with-text-memo*ceccon.me")
+    let with_memo_text = resolve_stellar_address("with-text-memo*stellar.ceccon.me")
         .await
         .unwrap();
 
     assert!(with_memo_text.memo.unwrap().is_text());
 
-    let with_memo_id = resolve_stellar_address("with-id-memo*ceccon.me")
+    let with_memo_id = resolve_stellar_address("with-id-memo*stellar.ceccon.me")
         .await
         .unwrap();
 
     assert!(with_memo_id.memo.unwrap().is_id());
 
-    let with_memo_hash = resolve_stellar_address("with-hash-memo*ceccon.me")
+    let with_memo_hash = resolve_stellar_address("with-hash-memo*stellar.ceccon.me")
         .await
         .unwrap();
 
